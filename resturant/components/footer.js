4a2200bb4120a47ecadf75837dea785ff15e6ba5
zark28 <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>
2022-08-24 14:19:41 +0000

    update2

--- resturant/components/footer.js
+++ resturant/components/footer.js
@@ -2,90 +2,66 @@ class Footer extends HTMLElement {
   constructor() {
     super();
   }
-
   connectedCallback() {
     this.innerHTML = `
-        <footer class="footer-area bg-f">
         <div class="container">
-          <div class="row">
-            <div class="col-lg-3 col-md-6">
-              <h3>About Us</h3>
-              <p>
-                The goal of the founders is to provide reliable source of tasty and affordable foods of various varities for people from all works of life
-              </p>
-            </div>
-            <div class="col-lg-3 col-md-6">
-              <h3>Opening hours</h3>
-              <p><span class="text-color">Monday: </span>Closed</p>
-              <p><span class="text-color">Tue-Wed :</span> 9:Am - 10PM</p>
-              <p><span class="text-color">Thu-Fri :</span> 9:Am - 10PM</p>
-              <p><span class="text-color">Sat-Sun :</span> 5:PM - 10PM</p>
-            </div>
-            <div class="col-lg-3 col-md-6">
-              <h3>Contact information</h3>
-              <p class="lead">Akropong-Akuapem
-                old level 400 premises a minute walk from the old council.</p>
-              <p class="lead"><a href="#">+233 244 020 570</a></p>
-              <p><a href="#"> conceptfoods@gmail.com</a></p>
-            </div>
-            <div class="col-lg-3 col-md-6">
-              <h3>Subscribe</h3>
-              <div class="subscribe_form">
-                <form class="subscribe_form">
-                  <input
-                    name="EMAIL"
-                    id="subs-email"
-                    class="form_input"
-                    placeholder="Email Address..."
-                    type="email"
-                  />
-                  <button type="submit" class="submit">SUBSCRIBE</button>
-                  <div class="clearfix"></div>
-                </form>
-              </div>
-              <ul class="list-inline f-social">
-                <li class="list-inline-item">
-                  <a href="#"
-                    ><i class="fa fa-facebook" aria-hidden="true"></i
-                  ></a>
-                </li>
-                <li class="list-inline-item">
-                  <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                </li>
-                <li class="list-inline-item">
-                  <a href="#"
-                    ><i class="fa fa-linkedin" aria-hidden="true"></i
-                  ></a>
-                </li>
-                <li class="list-inline-item">
-                  <a href="#"
-                    ><i class="fa fa-google-plus" aria-hidden="true"></i
-                  ></a>
-                </li>
-                <li class="list-inline-item">
-                  <a href="#"
-                    ><i class="fa fa-instagram" aria-hidden="true"></i
-                  ></a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-  
-        <div class="copyright">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-12">
-                <p class="company-name">
-                  All Rights Reserved. &copy; 2022
-                  <a href="#">Concept Foods</a>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
+			<div class="row row-pb-md">
+				<div class="col-md-4 fh5co-widget">
+					<h4>Concept Foods</h4>
+					<p>The Best Coffee & Restaurant in the East</p>
+				</div>
+				<div class="col-md-2 col-md-push-1 fh5co-widget">
+					<h4>Links</h4>
+					<ul class="fh5co-footer-links">
+						<li><a href="./index.html">Home</a></li>
+						<li><a href="./menu/html">Menu</a></li>
+						<li><a href="./gallery.html">Gallery</a></li>
+					</ul>
+				</div>
+
+				<div class="col-md-2 col-md-push-1 fh5co-widget">
+					<h4>Working Hours</h4>
+					<ul class="fh5co-footer-links">
+						<li>Weekdays </li>
+						<li>7am to 8pm</li>
+						<li><a href="#"></a></li>
+						<li><a href="#">Weekends</a></li>
+						<li>9am to 6pm</li>
+					</ul>
+				</div>
+
+				<div class="col-md-4 col-md-push-1 fh5co-widget">
+					<h4>Contact Information</h4>
+					<ul class="fh5co-footer-links">
+						<li>Akropong-Akuapem <br> old level 400 premises a minute walk from the old council.</li>
+						<li><a href="tel:0244020570">+233 244 020 570</a></li>
+						<li><a href="mailto:conceptfoods@gmail.com">conceptfoods@gmail.com</a></li>
+						<li><a href="http://https://">conceptfoods.bar</a></li>
+					</ul>
+				</div>
+
+			</div>
+
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2022 Concept Foods. All Rights Reserved.</small> 
+						<!-- <small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a></small> -->
+					</p>
+					<p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-twitter2"></i></a></li>
+							<li><a href="https://web.facebook.com/conceptsfoods/about" target='_blank' ><i class="icon-facebook2"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin2"></i></a></li>
+							<!-- <li><a href="#"><i class="icon-dribbble2"></i></a></li> -->
+						</ul>
+					</p>
+				</div>
+			</div>
+
+		</div>
         `;
   }
 }
+
 customElements.define('footer-component', Footer);
